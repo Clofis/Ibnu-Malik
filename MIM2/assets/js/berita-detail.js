4888ba070601f91b13720a3eefd9a76a88df9a92
Clofis <ummuhafidzchaniago@gmail.com>
2025-08-27 15:00:57 +0800

    Delete MIM2 directory

--- MIM2/assets/js/berita-detail.js
+++
@@ -1,8 +0,0 @@
-const navToggle = document.getElementById("nav-toggle");
-const navMenu = document.getElementById("nav-menu");
-
-if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-  });
-}
